92332a49e6d626fbd82a7e34f8bea1ba56da4295
Pramtoxz <pramuditometra@gmail.com>
2026-05-07 22:46:27 +0700

    build image dev

--- public/build/assets/text-link-BrhIDw8V.js
+++
@@ -1,1 +0,0 @@
-import{j as t,$ as n}from"./app-epU0mgTA.js";import{a}from"./createLucideIcon-DmuDasMy.js";function u({className:r="",children:e,...o}){return t.jsx(n,{className:a("text-foreground underline decoration-neutral-300 underline-offset-4 transition-colors duration-300 ease-out hover:decoration-current!",r),...o,children:e})}export{u as T};
