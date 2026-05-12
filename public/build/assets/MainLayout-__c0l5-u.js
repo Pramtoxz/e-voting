0362f6fa8a4c11a1fbc343dd92f107086152a895
Rafi Chandra <crafi2606@gmail.com>
2026-05-12 10:51:06 +0700

    live

---
+++ public/build/assets/MainLayout-__c0l5-u.js
@@ -0,0 +1,1 @@
+import{j as s}from"./app-DFX87-f8.js";import{N as i,F as m}from"./Navbar-Dhi6gDMC.js";import"./avatar-omovI6kU.js";function t({children:e,auth:r,showFooter:o=!0}){return s.jsxs("div",{className:"flex min-h-screen flex-col bg-gray-50",children:[s.jsx(i,{user:r==null?void 0:r.user}),e,o&&s.jsx(m,{})]})}export{t as M};
